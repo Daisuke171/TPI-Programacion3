04e3a7a5845a698b43c40e1ab28e15bac3c62246
Eloy Meza <eloymeza111@gmail.com>
2025-11-13 15:52:43 -0300

    Agregué la clase Turno

---
+++ Entidades/Turno.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Turno
+    {
+        public Turno() { }
+        public Turno(DateTime fecha, int legajoMedico, string dniPaciente, string asistencia, string observacion)
+        {
+            _fecha = fecha;
+            _legajoMedico = legajoMedico;
+            _dniPaciente = dniPaciente;
+            _asistencia = asistencia;
+            _observacion = observacion;
+        }
+
+        public DateTime _fecha { get; set; }
+        public int _legajoMedico { get; set; }
+        public string _dniPaciente { get; set; }
+        public string _asistencia { get; set; }
+        public string _observacion { get; set; }
+        public bool _estadoTurno { get; set; } = true;
+    }
+}
